29a9f6585f52d344fc66a632c8a61c3d1df7b3b0
stefan <ronczka@hm.edu>
2019-07-03 07:41:08 +0000

    change the way trump faces gets filtered by using a histogram comparison on a reference with a treshhold

--- filter_trump.py
+++ filter_trump.py
@@ -4,23 +4,33 @@ import numpy as np
 from shutil import copy2
 
 if __name__=='__main__':
-    trump_image_path = 'trump.jpg'
-    trump_related_image_path = 'trump_related.jpg'
-    trump_image = Image.open(trump_image_path)
-    trump_image = np.array(trump_image.getdata())
-    trump_related_image = Image.open(trump_related_image_path)
-    trump_related_image= np.array(trump_related_image.getdata())
 
-    results_path = 'C:/Users/stefa/Videos/GAN_deep_fakes/results'
-    images_path = 'C:/Users/stefa/Videos/GAN_deep_fakes/trump_data'
+    max_epsilon= 0.07
+    trump_image_path = 'trump.jpg'
+
+    trump_image = Image.open(trump_image_path)
+    
+
+    results_path = 'filtered_faces'
+    images_path = './faceB/rgb'
+
+    os.makedirs(results_path, exist_ok=True)
     image_paths = os.listdir(images_path)
+
+    trump_hist = np.array(trump_image.histogram()) / (trump_image.width * trump_image.height * 3)
     for i in range(len(image_paths)):
         image_path = os.path.join(images_path, image_paths[i])
         image = Image.open(image_path)
+        image_size = image.size
+        #print(image_size)
         image_array = np.array(image.getdata())
-        diff_trump = sum(sum(abs(image_array - trump_image)))
-        diff_related = sum(sum(abs(image_array - trump_related_image)))
-        if diff_trump < diff_related:
+        #trump_image_resized = trump_image.resize(image_size)
+        #trump_image_resized = np.array(trump_image_resized.getdata())
+        image_hist = np.array(image.histogram()) / (image.width * image.height * 3)
+        diff_trump = np.linalg.norm(trump_hist - image_hist, ord=2, axis=0)
+        print(diff_trump)
+        # use a the color histogram for scanning true images containing a trump like colors
+        if diff_trump < max_epsilon:
             copy2(image_path, results_path)
         image.close()
 
